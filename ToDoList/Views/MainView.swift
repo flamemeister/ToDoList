daf7a69666bb081428b4f74f7f905d3e97c2cd0c
flamemeister <aldiyar.saken123@gmail.com>
2023-12-15 16:29:40 +0100

    tab bar

--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -12,7 +12,16 @@ struct MainView: View {
     
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            ToDoListView()
+            TabView{
+                ToDoListView()
+                    .tabItem{
+                        Label("Home", systemImage: "house")
+                    }
+                ProfileView()
+                    .tabItem{
+                        Label("Profile", systemImage: "person.circle")
+                    }
+            }
         } else {
              LoginView()
         }
