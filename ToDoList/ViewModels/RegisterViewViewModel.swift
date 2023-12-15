d4413a0ac4f1fc860682dd069d25e86fe3af211d
flamemeister <aldiyar.saken123@gmail.com>
2023-12-15 14:35:39 +0100

    registration, insert function and user model creation

--- ToDoList/ViewModels/RegisterViewViewModel.swift
+++ ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -5,4 +5,45 @@
 //  Created by Aldiyar Saken on 10.12.2023.
 //
 
+import FirebaseAuth
 import Foundation
+
+class RegisterViewViewModel: ObservableObject{
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    init() {}
+    
+    func register(){
+        guard validate() else{
+            return
+        }
+        Auth.auth().createUser(withEmail: email, password: password){
+            [weak self] result, error in
+            guard let userId = result?.user.uid else{
+                return
+            }
+            self?.insertUserRecord(id: userId)
+        }
+    }
+    
+    private func insertUserRecord(id: String){
+        let newUser = User
+    }
+    
+    private func validate() -> Bool{
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        guard email.contains("@") && email.contains(".") else{
+            return false
+        }
+        guard password.count >= 6 else{
+            return false
+        }
+        return true
+    }
+}
