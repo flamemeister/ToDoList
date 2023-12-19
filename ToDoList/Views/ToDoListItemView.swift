f3299e26ccdd4828d95e55a38b32d58baf7c7509
flamemeister <aldiyar.saken123@gmail.com>
2023-12-19 16:55:27 +0100

    toogleIsDone function

--- ToDoList/Views/ToDoListItemView.swift
+++ ToDoList/Views/ToDoListItemView.swift
@@ -23,7 +23,8 @@ struct ToDoListItemView: View {
             Button{
                 viewModel.toggleIsDone(item: item)
             } label: {
-                Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                Image(systemName: item.isDone ?
+                      "checkmark.circle.fill" : "circle")
             }
         }
     }
