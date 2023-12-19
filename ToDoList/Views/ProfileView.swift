60b22a691fd7bf7923b8ec06e8eddab9155e81be
flamemeister <aldiyar.saken123@gmail.com>
2023-12-19 17:26:54 +0100

    fetchUser function

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -8,18 +8,56 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
+    
     var body: some View {
         NavigationView{
             VStack{
+                //avatar
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.blue)
+                    .frame(width: 125, height: 125)
+                    .padding()
                 
+                VStack(alignment: .leading){
+                    HStack{
+                        Text("Name: ")
+                            .bold()
+                        Text("Aldiyar Saken")
+                    }
+                    .padding()
+                    HStack{
+                        Text("Email ")
+                            .bold()
+                        Text("aldiyar.saken123@gmail.com")
+                    }
+                    .padding()
+                    HStack{
+                        Text("Member Since: ")
+                            .bold()
+                        Text("19th December, 2023")
+                    }
+                    .padding()
+                }
+                    .padding()
+                    
+                    Button("Log Out"){
+                        viewModel.logOut()
+                    }
+                    .tint(.red)
+                    .padding()
+                    Spacer()
+                }
+                .navigationTitle("Profile")
             }
-            .navigationTitle("Profile")
         }
     }
-}
-
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
+    
+    struct ProfileView_Previews: PreviewProvider {
+        static var previews: some View {
+            ProfileView()
+        }
     }
-}
+
